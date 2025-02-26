92a42d43b164bd252d015262aa1374afa742fca5
Diego <diego.wartbuechler0@gmail.com>
2025-02-26 11:46:48 +0100

    Funzioni completate

--- c++/array bidimensionali/bidimensionali.h
+++ c++/array bidimensionali/bidimensionali.h
@@ -1,20 +1,20 @@
 #include <iostream>
 using namespace std;
-const int  num_colonne=5;//colonne
-const int  num_righe=6; //righe
+const int num_colonne = 5; // colonne
+const int num_righe = 6;   // righe
 
 /**
  * @brief Initializes a 2D array of strings by setting all elements to an empty string.
- * 
+ *
  * @param libreria A 2D array of strings with dimensions [num_righe][num_colonne].
  */
 void inizializzaLibreria(string libreria[num_righe][num_colonne])
 {
     for (int i = 0; i < num_righe; i++)
     {
-        for (int j = 0 ; j < num_colonne; j++)
+        for (int j = 0; j < num_colonne; j++)
         {
-            libreria[i][j]="";
+            libreria[i][j] = " ";
         }
     }
     return;
@@ -22,46 +22,69 @@ void inizializzaLibreria(string libreria[num_righe][num_colonne])
 
 /**
  * @brief Aggiunge un libro alla libreria nella posizione specificata.
- * 
+ *
  * Questa funzione aggiunge un libro alla libreria in una posizione specificata
  * da numScaffale e numPosizione. Se i valori di numScaffale o numPosizione sono
- * fuori dai limiti, viene richiesto all'utente di inserirli nuovamente. Se la 
- * posizione specificata è già occupata da un altro libro, viene visualizzato un 
+ * fuori dai limiti, viene richiesto all'utente di inserirli nuovamente. Se la
+ * posizione specificata è già occupata da un altro libro, viene visualizzato un
  * messaggio di avviso. Altrimenti, il libro viene aggiunto alla libreria.
- * 
+ *
  * @param libreria La matrice che rappresenta la libreria.
  * @param numScaffale Il numero dello scaffale in cui aggiungere il libro.
  * @param numPosizione La posizione nello scaffale in cui aggiungere il libro.
  * @param libro Il titolo del libro da aggiungere.
  */
 
-void aggiungiLibro(string libreria[num_righe][num_colonne], int numScaffale,int numPosizione, string libro)
+void aggiungiLibro(string libreria[num_righe][num_colonne], int numScaffale, int numPosizione, string libro)
 {
-    while ((numScaffale<0||numScaffale>=num_righe) || (numPosizione<0||numPosizione>=num_colonne))
+    while ((numScaffale < 0 || numScaffale >= num_righe) || (numPosizione < 0 || numPosizione >= num_colonne))
     {
-        cout<<"\n Valori sbagliati, inserisci num scaffale: ";
-        cin>>numScaffale;
-        cout<<"\n Valori sbagliati, inserisci numPosizione: ";
-        cin>>numPosizione;
+        cout << "\n Valori sbagliati, inserisci num scaffale: ";
+        cin >> numScaffale;
+        cout << "\n Valori sbagliati, inserisci numPosizione: ";
+        cin >> numPosizione;
     }
-    if (libreria[numScaffale][numPosizione]!= "")
+    if (libreria[numScaffale][numPosizione] != "")
     {
-        cout<<"\n In questa posizione c'e' gia' un libro: "<<libreria[numScaffale][numPosizione];
+        cout << "\n In questa posizione c'e' gia' un libro: " << libreria[numScaffale][numPosizione];
     }
     else
     {
-        libreria[numScaffale][numPosizione]=libro;
-        cout<<"\n Libro aggiunto "<<libreria[numScaffale][numPosizione];      
+        libreria[numScaffale][numPosizione] = libro;
+        cout << "\n Libro aggiunto " << libreria[numScaffale][numPosizione];
     }
     return;
 }
 
 void stampaLibreria(string libreria[num_righe][num_colonne])
 {
-    
+    for (int i = 0; i < num_righe; i++)
+    {
+        for (int j = 0; i < num_colonne; j++)
+        {
+            cout << libreria[i][j] << " ";
+        }
+    }
+    return;
 }
 
-void rimuoviLibro(string libreria[num_righe][num_colonne], int numScaffale,int numPosizione)
+void rimuoviLibro(string libreria[num_righe][num_colonne], int numScaffale, int numPosizione, string libro)
 {
-    
+    while ((numScaffale < 0 || numScaffale >= num_righe) || (numPosizione < 0 || numPosizione >= num_colonne))
+    {
+        cout << "\n Valori sbagliati, inserisci num scaffale: ";
+        cin >> numScaffale;
+        cout << "\n Valori sbagliati, inserisci numPosizione: ";
+        cin >> numPosizione;
+    }
+    if (libreria[numScaffale][numPosizione] != libro)
+    {
+        cout << "\n In questa posizione non c'è nessun libro: " << libreria[numScaffale][numPosizione];
+    }
+    else
+    {
+        libreria[numScaffale][numPosizione] = libro;
+        cout << "\n Libro rimosso" << libreria[numScaffale][numPosizione];
+    }
+    return;
 }
