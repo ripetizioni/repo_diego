ceb9e4ca2b8464413111f3a77c7466baac3daf05
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-03 22:11:48 +0100

    esercizio quasi finito, manca la struct per le classi di una scuola

--- c++/struct/main.cpp
+++ c++/struct/main.cpp
@@ -3,7 +3,79 @@
 using namespace std;
 
 int main() {
-    Studente studenti[2] = {{"Mario", "Rossi", 3}, {"Luigi", "Verdi", 4}};
-    stampaTuttiStudenti(studenti, 2);
+    // Dichiarazione di un array di strutture Studente di dimensione 50
+    struct Studente classe[50];
+    // Variabile per tenere traccia della dimensione logica dell'array
+    int dimensioneLogica=0;
+    // Variabile per memorizzare la scelta dell'utente
+    char scelta;
+    do
+    {
+        // Menu di scelta per l'utente
+        cout<< "\ninserisci 'a' per aggiungere uno studente, \n'v' per aggiungere un voto a uno studente, \n's' per stampare la classe,\n't' per sapere lo studente con la media migliore, \n'q' per uscire\n";
+        cin>>scelta;
+        switch (scelta)
+        {
+            case 'a':
+            {
+                // Aggiunta di un nuovo studente
+                cout<<"\n Inserisci Nome: ";
+                cin>>classe[dimensioneLogica].nome;
+                cout<<"\n Inserisci Congnome: ";
+                cin>>classe[dimensioneLogica].cognome;
+                cout<<"\nInserisci classe: ";
+                cin>>classe[dimensioneLogica].classe;
+                cout<<"\nAggiunto studente ";
+                // Stampa dei dettagli dello studente appena aggiunto
+                stampaStudente(classe[dimensioneLogica]);
+                // Incremento della dimensione logica
+                dimensioneLogica++;
+                break;
+            }
+            case 'v':
+            {
+                // Aggiunta di un voto a uno studente esistente
+                int studente;
+                float voto;
+                // Stampa della lista degli studenti
+                stampaClasse(classe,dimensioneLogica);
+                cout<<"\n A quale studente vuoi aggiugnere il voto: ";
+                cin>>studente;
+                cout<<"\nInserisci voto: ";
+                cin>>voto;
+                // Aggiunta del voto allo studente selezionato
+                classe[studente].voti[classe[studente].dimensioneLogica]=voto;
+                classe[studente].dimensioneLogica++;
+                cout<<"\nVoto aggiunto ";
+                break;
+            }
+            case 's':
+            {
+                // Stampa della lista completa degli studenti
+                stampaClasse(classe,dimensioneLogica);
+                break;
+            }
+            case 't':
+            {
+                // Stampa dello studente con la media dei voti migliore
+                cout<<"\nLo studente con la media migliore è: ";
+                stampaStudente(miglioreStudente(classe,dimensioneLogica));
+                break;
+            }
+            case 'q':
+            {
+                // Uscita dal programma
+                cout<<"\n Uscita del programma";
+                break;
+            }
+            default:
+            {
+                // Gestione della scelta non valida
+                cout<<"\nScelta non valida";
+                break;
+            }
+        }
+    }while (scelta!='q'); // Ripeti il ciclo finché l'utente non sceglie di uscire ('q')
+    
     return 0;
 }
--- c++/struct/studente.h
+++ c++/struct/studente.h
@@ -4,28 +4,102 @@
 using namespace std;
 
 struct Studente
+/**
+ * @brief Struct representing a student.
+ * 
+ * This struct contains information about a student, including their name, surname,
+ * class, grades, and the logical size of the grades array.
+ * 
+ * @var nome The first name of the student.
+ * @var cognome The last name of the student.
+ * @var classe The class or grade level of the student.
+ * @var voti An array of grades for the student, with a maximum size of 50.
+ * @var dimensioneLogica The logical size of the grades array, indicating the number of grades actually stored.
+ */
 {
     string nome;
     string cognome;
     int classe;
+    float voti[50];
+    int dimensioneLogica=0;
+
 };
 
 void stampaStudente(struct Studente A)
 {
-    cout<<"\n"<<A.nome<<"\n"<<A.cognome<<"\n"<<A.classe;
+    cout<<"\nNome: "<<A.nome<<" Cognome: "<<A.cognome<<" Classe: "<<A.classe;
     return;
 }
 
+/**
+ * @brief Stampa le informazioni di una classe di studenti.
+ * 
+ * Questa funzione itera attraverso un array di strutture Studente e stampa le informazioni
+ * di ciascuno studente utilizzando la funzione stampaStudente.
+ * 
+ * @param classe Array di strutture Studente che rappresenta la classe.
+ * @param dimensione Numero di studenti nella classe.
+ */
 void stampaClasse(struct Studente classe[], int dimensione)
 {
     for (int i = 0; i < dimensione; i++)
     {
-        cout<<"\n";
+        cout<<"\n Studente numero: "<<i;
         stampaStudente(classe[i]);
     }
     return;
 }
 
+/**
+ * @brief Calcola la media dei voti di uno studente.
+ * 
+ * Questa funzione prende in input una struttura Studente e calcola la media
+ * dei voti contenuti nell'array voti della struttura.
+ * 
+ * @param A La struttura Studente contenente i voti e la dimensione logica.
+ * @return La media dei voti come valore float.
+ */
+float calcolaMedia(struct Studente A)
+{
+    float somma = 0;
+    for (int i = 0; i < A.dimensioneLogica; i++)
+    {
+        somma=somma+A.voti[i];
+    }
+    somma=somma/A.dimensioneLogica;
+    return somma;
+}
 
+
+/**
+ * @brief Trova lo studente con la media più alta in un array di studenti.
+ *
+ * Questa funzione itera attraverso un array di strutture Studente e
+ * determina quale studente ha la media più alta. Restituisce una copia
+ * della struttura Studente con la media più alta.
+ *
+ * @param A Array di strutture Studente.
+ * @param dimensione Numero di elementi nell'array A.
+ * @return La struttura Studente con la media più alta.
+ */
+struct Studente miglioreStudente(struct Studente A[], int dimensione)
+{
+    // Inizializza il miglior studente come il primo della lista
+    struct Studente migliore=A[0];
+    
+    // Itera attraverso tutti gli studenti
+    for(int i=1; i<dimensione;i++)
+    {
+        // Se la media del miglior studente è inferiore alla media dello studente corrente
+        if(calcolaMedia(migliore)<calcolaMedia(A[i]))
+        {
+            // Aggiorna il miglior studente con lo studente corrente
+            migliore=A[i];
+        }
+    }
+    
+    // Ritorna il miglior studente
+    return migliore;
+}
 
 
